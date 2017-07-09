e4bba5b0a58fa95c860f4bb2bc2027b7de67ec1a
Peisheng YE <peisheng.ye.88@gmail.com>
2017-07-09 07:17:59 -0400

    broadcast receiver/ intent service to havelocation update code

---
+++ app/src/main/java/com/radioyps/travelreminder/AlarmReciver.java
@@ -0,0 +1,17 @@
+package com.radioyps.travelreminder;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+/**
+ * Created by yep on 08/07/17.
+ */
+
+public class AlarmReciver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+    }
+}
